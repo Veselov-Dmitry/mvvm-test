741c2c4a3dbd4eba71dc3b4c8808324ab44de4a5
TDA <tda777@tut.by>
2017-05-05 16:20:35 +0300

    first

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using MVVMTest.View;
+using MVVMTest.ViewModel;
+
+namespace MVVMTest
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var mw = new MainWindowView 
+            {
+                DataContext = new MainViewModel()
+            };
+
+            mw.Show();
+        }
+    }
+}
